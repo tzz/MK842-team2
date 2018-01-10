d027c1a79f579c6eadde0419c1bcd293362f1f8f
Ted Zlatanov <tzz@lifelogs.com>
2018-01-10 16:20:21 -0500

    update

--- airbnb.R
+++ airbnb.R
@@ -1,5 +1,10 @@
 #!/usr/bin/env Rscript
 print("Hello")
 library(bit64)
+library(data.table)
+library(ggplot2)
+library(scales)
+theme_set(theme_bw())
+
 script.dir <- dirname(sys.frame(1)$ofile)
 air <- fread(sprintf("%s/%s", script.dir, "listings.csv"))
