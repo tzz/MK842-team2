9ecfcca0a062e7c8e05dd6e90534ef67cc5f7bbc
Ted Zlatanov <tzz@lifelogs.com>
2018-01-11 16:49:08 -0500

    clean up R and add binary data sets for faster loading

--- airbnb.R
+++ airbnb.R
@@ -1,13 +1,31 @@
 #!/usr/bin/env Rscript
-print("Hello")
+
+print("MK842 Team 2: AirBnB Analysis Project")
+
+print("Loading libraries")
 library(bit64)
 library(data.table)
 library(ggplot2)
 library(scales)
+library(stringr)
 theme_set(theme_bw())
 
 script.dir <- dirname(sys.frame(1)$ofile)
-air <- fread(sprintf("%s/%s", script.dir, "listings.csv"), stringsAsFactors = TRUE)
 
-data.table(air)
+if (0) {
+  print("Creating training and test data sets from a 50/50 split of the CSV")
+  air <- read.csv(sprintf("%s/%s", script.dir, "listings.csv"), stringsAsFactors = TRUE)
 
+  airt <- data.table(air)
+
+  atrain.sampled <- sample(nrow(airt), nrow(airt)/2)
+  atrain <- airt[atrain.sampled]
+  atest <- airt[!atrain.sampled]
+  print("Saving training and test data sets")
+  save(atest, file=sprintf("%s/%s", script.dir, "atest.dat"))
+  save(atrain, file=sprintf("%s/%s", script.dir, "atrain.dat"))
+} else {
+  print("Loading training and test data sets from binary storage. Note the atest and atrain variables will be OVERWRITTEN")
+  load(file=sprintf("%s/%s", script.dir, "atrain.dat"))
+  load(file=sprintf("%s/%s", script.dir, "atest.dat"))
+}
