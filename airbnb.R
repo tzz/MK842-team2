29fc102b6d1e330283dda41615a38b25670ba0d0
Ted Zlatanov <tzz@lifelogs.com>
2018-01-11 14:23:05 -0500

    add stringsAsFactors=TRUE

--- airbnb.R
+++ airbnb.R
@@ -7,4 +7,4 @@ library(scales)
 theme_set(theme_bw())
 
 script.dir <- dirname(sys.frame(1)$ofile)
-air <- fread(sprintf("%s/%s", script.dir, "listings.csv"))
+air <- fread(sprintf("%s/%s", script.dir, "listings.csv"), stringsAsFactors = TRUE)
